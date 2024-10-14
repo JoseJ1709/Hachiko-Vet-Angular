c43d36f82386621df98eb90472d3b45a44d98f72
bananna2 <118637827+bananna2@users.noreply.github.com>
2024-10-13 23:34:07 -0500

    diseno wip

--- src/app/components/client/login/login.component.spec.ts
+++ src/app/components/client/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { LoginComponentClient } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let component: LoginComponentClient;
+  let fixture: ComponentFixture<LoginComponentClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponentClient]
     });
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(LoginComponentClient);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/dogs/login/login.component.spec.ts
+++ src/app/components/dogs/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { LoginComponentDogs } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let component: LoginComponentDogs;
+  let fixture: ComponentFixture<LoginComponentDogs>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponentDogs]
     });
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(LoginComponentDogs);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
